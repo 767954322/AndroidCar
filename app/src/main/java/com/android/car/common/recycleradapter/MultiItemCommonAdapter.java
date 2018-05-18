f0202eab7198b0ed732f5cab059971d15f3762e6
gumenghao <18911211362@163.com>
2018-05-19 04:02:12 +0800

    添加首页假数据

---
+++ app/src/main/java/com/android/car/common/recycleradapter/MultiItemCommonAdapter.java
@@ -0,0 +1,37 @@
+package com.android.car.common.recycleradapter;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by lvr on 2017/5/24.
+ */
+
+public abstract class MultiItemCommonAdapter<T> extends CommonAdapter<T> {
+    protected MultiItemTypeSupport<T> mMultiItemTypeSupport;
+
+    public MultiItemCommonAdapter(Context context, List<T> datas,
+                                  MultiItemTypeSupport<T> multiItemTypeSupport) {
+        super(context, -1, datas);
+        mMultiItemTypeSupport = multiItemTypeSupport;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mMultiItemTypeSupport.getItemViewType(position, mDatas.get(position));
+    }
+
+    @Override
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        int layoutId = mMultiItemTypeSupport.getLayoutId(viewType);
+        BaseViewHolder holder = new BaseViewHolder(mContext, parent, layoutId);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(BaseViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+    }
+}
--- app/src/main/java/com/android/car/home/ui/fragment/FirstFragment.java
+++ app/src/main/java/com/android/car/home/ui/fragment/FirstFragment.java
@@ -2,7 +2,12 @@ package com.android.car.home.ui.fragment;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +15,18 @@ import android.view.ViewGroup;
 
 import com.android.car.R;
 import com.android.car.common.base.BaseFragment;
+import com.android.car.common.recycleradapter.BaseViewHolder;
+import com.android.car.common.recycleradapter.MultiItemCommonAdapter;
+import com.android.car.common.recycleradapter.MultiItemTypeSupport;
 import com.android.car.common.viewpagerscroll.ConvenientBanner;
 import com.android.car.common.viewpagerscroll.NetworkImageHolderView;
 import com.android.car.common.viewpagerscroll.holder.CBViewHolderCreator;
 import com.android.car.common.viewpagerscroll.listener.OnItemClickListener;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
 import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +40,7 @@ import butterknife.Unbinder;
  */
 
 @SuppressLint("ValidFragment")
-public class FirstFragment extends BaseFragment implements View.OnClickListener {
+public class FirstFragment extends BaseFragment implements View.OnClickListener, OnLoadMoreListener {
 
     @BindView(R.id.convenientBanner)
     ConvenientBanner mConvenientBanner;
@@ -43,12 +53,17 @@ public class FirstFragment extends BaseFragment implements View.OnClickListener 
 
     private FragmentManager fragmentManager;
     private List<String> imageUrlList = new ArrayList<>();
+    private List<String> dataList = new ArrayList<>();
+    private MultiItemCommonAdapter<String> mAdapter;
 
     {
         imageUrlList.add("http://pic4.nipic.com/20091113/2847083_105626034638_2.jpg");
         imageUrlList.add("http://f2.topitme.com/2/6a/bc/113109954583dbc6a2o.jpg");
         imageUrlList.add("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2137958015,4291978384&fm=27&gp=0.jpg");
         imageUrlList.add("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3102711909,683345632&fm=27&gp=0.jpg");
+
+        for (int i = 0; i < 10; i++)
+            dataList.add("");
     }
 
     public FirstFragment(FragmentManager fragmentManager) {
@@ -68,6 +83,7 @@ public class FirstFragment extends BaseFragment implements View.OnClickListener 
     protected void initListener() {
         super.initListener();
 
+        mRefreshLayout.setOnLoadMoreListener(this);
 
     }
 
@@ -79,6 +95,29 @@ public class FirstFragment extends BaseFragment implements View.OnClickListener 
         mClassicsFooter = (ClassicsFooter) mRefreshLayout.getRefreshFooter();
         mClassicsFooter.setSpinnerStyle(SpinnerStyle.Translate);
         mRefreshLayout.setFooterHeight(50);
+
+        MultiItemTypeSupport<String> support = new MultiItemTypeSupport<String>() {
+            @Override
+            public int getLayoutId(int itemType) {
+                return R.layout.item_shouye;
+            }
+
+            @Override
+            public int getItemViewType(int position, String s) {
+                return 0;
+            }
+        };
+        mAdapter = new MultiItemCommonAdapter<String>(activity, dataList, support) {
+
+            @Override
+            public void convert(BaseViewHolder holder, int position) {
+
+            }
+        };
+        rvRecyclerview.setLayoutManager(new LinearLayoutManager(activity));
+        rvRecyclerview.setItemAnimator(new DefaultItemAnimator());
+        rvRecyclerview.setNestedScrollingEnabled(false);
+        rvRecyclerview.setAdapter(mAdapter);
     }
 
     @Override
@@ -87,6 +126,13 @@ public class FirstFragment extends BaseFragment implements View.OnClickListener 
         switch (v.getId()) {
 
         }
+
+    }
+
+    @Override
+    public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+
+        handler.sendEmptyMessageDelayed(0, 1500);
 
     }
 
@@ -117,5 +163,18 @@ public class FirstFragment extends BaseFragment implements View.OnClickListener 
                     }
                 });
     }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            for (int i = 0; i < 10; i++)
+                dataList.add("");
+            mRefreshLayout.finishLoadMore();
+            mAdapter.notifyDataSetChanged();
+
+        }
+    };
 
 }
